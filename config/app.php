d514ae474a005923ac2ea41b7b8f81d53f50aa44
ZaidAbukhalaf2 <zaid.ammar.ak@gmail.com>
2023-01-07 18:18:58 +0300

    lats_push

--- config/app.php
+++ config/app.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\App;
 
 return [
 
@@ -174,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\RiakServiceProvider::class,
 
     ],
 
